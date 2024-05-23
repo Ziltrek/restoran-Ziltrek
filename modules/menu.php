10be1e6f4ffd0943fd9574f1ae75daff062843d8
Ziltrek <ivan.vanya.2006@yandex.ru>
2024-05-23 21:59:14 +0300

    Add files via upload

---
+++ modules/menu.php
@@ -0,0 +1,67 @@
+<div class="header">
+    <div class="container">
+        <div class="line">
+            <div class="logo">
+                <img src="img/LOGO.png" alt="#">
+            </div>
+            <nav>
+                <a class="navitem" href="">ГЛАВНЯ</a>
+                <a class="navitem" href="">МЕНЮ</a>
+                <a class="navitem" href="">О НАС</a>
+                <a class="navitem" href="">БРОНЬ</a>
+            </nav>
+
+            <div class="cart">
+                <a href="#">
+                    <img class="cartimg" src="img/cart.png" alt="">
+                </a>
+            </div>
+
+            <div class="phone">
+                <div class="phoneholder">
+                    <div class="phoneimg">
+                        <img src="img/phone.png" alt="">
+                    </div>
+                    <div class="namber">
+                        <a class="num" href="#">
+                            +912-345-67-89
+                        </a>
+                    </div>
+                </div>
+                <div class="phonetext">
+                    Свяжитесь с нами для <br> бронирования
+                </div>
+            </div> 
+            <div class="button">
+                <a href="#">
+                    ЗАКАЗ СТОЛИКА
+                </a>
+            </div>
+
+        </div>
+
+
+        <div class="headerdown">
+            <div class="headertitle">
+                Добро пожаловать в
+                <div class="headersubtitle">
+                    Наш ресторан
+                </div>
+
+
+                <div class="headersuptitle">ДОМ ЛУЧШЕЙ ЕДЫ</div>
+            
+
+
+                <div class="headerbth">
+                    <a href="#" class="headerbutton">
+                        VIEW MENU
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    
+</div>
